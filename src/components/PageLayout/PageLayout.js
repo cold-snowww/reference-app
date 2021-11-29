f7f87c3c368d1fec459f8b436f9fd8105e6db06f
Roman Kolmakov <cold.snowww@yahoo.com>
2021-11-29 14:09:55 +0700

    Add scrolled containers

--- src/components/PageLayout/PageLayout.js
+++ src/components/PageLayout/PageLayout.js
@@ -11,17 +11,19 @@ export default function PageLayout({
 }) {
    return (
       <div className="PageLayout">
-         <div className="PageLayout__burger">{burger} burger</div>
-         <div className="PageLayout__controls">{controls} controls</div>
+         <div className="PageLayout__burger">{burger}</div>
+         <div className="PageLayout__controls">{controls}</div>
          <div className="PageLayout__page">
-            <div className="PageLayout__pageWrapper">{page} page</div>
+            <div className="PageLayout__wrapper">{page}</div>
          </div>
          <div className="PageLayout__prompt">
-            <div className="PageLayout__promptWrapper">{prompt} prompt</div>
+            <div className="PageLayout__wrapper">{prompt}</div>
          </div>
-         <div className="PageLayout__header">{header} header</div>
-         <div className="PageLayout__footer">{footer} footer</div>
-         <div className="PageLayout__menu">{menu} menu</div>
+         <div className="PageLayout__header">{header}</div>
+         <div className="PageLayout__footer">{footer}</div>
+         <div className="PageLayout__menu">
+            <div className="PageLayout__wrapper">{menu}</div>
+         </div>
       </div>
    );
 }
