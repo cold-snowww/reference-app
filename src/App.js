99f2a191f2a51db05c51ac8245ff5b1cc999f0a4
Roman Kolmakov <cold.snowww@yahoo.com>
2021-12-03 14:42:29 +0700

    Replace BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import WelcomeScreen from './components/WelcomeScreen/WelcomeScreen';
 import { Routes, Route } from 'react-router-dom';
 import PageScreen from './components/PageScreen/PageScreen';
 import reactData from './data/react.json';
 import reactPatternsData from './data/react-patterns.json';
 import redux from './data/redux.json';
-import Header from './components/Header/Header';
 
 function App() {
    return (
-      <>
-         <BrowserRouter>
-            <Routes>
-               <Route
-                  path="https://cold-snowww.github.io/reference-app/"
-                  element={<WelcomeScreen title="Welcome to Kinsey app!" />}
-               />
-               <Route
-                  path="https://cold-snowww.github.io/reference-app/react"
-                  element={<PageScreen data={reactData} title="React JS" />}
-               />
-               <Route
-                  path="https://cold-snowww.github.io/reference-app/react-patterns"
-                  element={
-                     <PageScreen
-                        data={reactPatternsData}
-                        title="React Patterns"
-                     />
-                  }
-               />
-               <Route
-                  path="https://cold-snowww.github.io/reference-app/redux"
-                  element={<PageScreen data={redux} title="Redux JS" />}
-               />
-            </Routes>
-         </BrowserRouter>
-         <Header />
-      </>
+      <HashRouter>
+         <Routes>
+            <Route
+               path="/"
+               element={<WelcomeScreen title="Welcome to Kinsey app!" />}
+            />
+            <Route
+               path="/react"
+               element={<PageScreen data={reactData} title="React JS" />}
+            />
+            <Route
+               path="/react-patterns"
+               element={
+                  <PageScreen data={reactPatternsData} title="React Patterns" />
+               }
+            />
+            <Route
+               path="/redux"
+               element={<PageScreen data={redux} title="Redux JS" />}
+            />
+         </Routes>
+      </HashRouter>
    );
 }
 
