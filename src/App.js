e1871f156780399077752b9218116fa550502e01
Roman Kolmakov <cold.snowww@yahoo.com>
2021-12-03 14:19:41 +0700

    .

--- src/App.js
+++ src/App.js
@@ -6,31 +6,38 @@ import PageScreen from './components/PageScreen/PageScreen';
 import reactData from './data/react.json';
 import reactPatternsData from './data/react-patterns.json';
 import redux from './data/redux.json';
+import Header from './components/Header/Header';
 
 function App() {
    return (
-      <BrowserRouter>
-         <Routes>
-            <Route
-               path="https://cold-snowww.github.io/reference-app/"
-               element={<WelcomeScreen title="Welcome to Kinsey app!" />}
-            />
-            <Route
-               path="https://cold-snowww.github.io/reference-app/react"
-               element={<PageScreen data={reactData} title="React JS" />}
-            />
-            <Route
-               path="https://cold-snowww.github.io/reference-app/react-patterns"
-               element={
-                  <PageScreen data={reactPatternsData} title="React Patterns" />
-               }
-            />
-            <Route
-               path="https://cold-snowww.github.io/reference-app/redux"
-               element={<PageScreen data={redux} title="Redux JS" />}
-            />
-         </Routes>
-      </BrowserRouter>
+      <>
+         <BrowserRouter>
+            <Routes>
+               <Route
+                  path="https://cold-snowww.github.io/reference-app/"
+                  element={<WelcomeScreen title="Welcome to Kinsey app!" />}
+               />
+               <Route
+                  path="https://cold-snowww.github.io/reference-app/react"
+                  element={<PageScreen data={reactData} title="React JS" />}
+               />
+               <Route
+                  path="https://cold-snowww.github.io/reference-app/react-patterns"
+                  element={
+                     <PageScreen
+                        data={reactPatternsData}
+                        title="React Patterns"
+                     />
+                  }
+               />
+               <Route
+                  path="https://cold-snowww.github.io/reference-app/redux"
+                  element={<PageScreen data={redux} title="Redux JS" />}
+               />
+            </Routes>
+         </BrowserRouter>
+         <Header />
+      </>
    );
 }
 
